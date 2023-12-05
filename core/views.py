d0a081b43c360ab385db0d26fc20a92974797b2f
Roman Podolets <romanpyu@gmail.com>
2023-12-06 00:34:59 +0200

    Added form to view function

--- core/views.py
+++ core/views.py
@@ -1,3 +1,34 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from core.forms import CommentaryForm
+from core.models import Commentary, User
+
+
+def commentary_list(request: HttpRequest) -> HttpResponse:
+    commentaries = Commentary.objects.all()
+    commentary_form = CommentaryForm()
+
+    if request.method == "POST":
+        comment_form = CommentaryForm(data=request.POST)
+        if comment_form.is_valid():
+            results = comment_form.cleaned_data
+            user = User.objects.create(
+                username=results["username"],
+                email=results["email"],
+                home_page=results["home_url"]
+            )
+            Commentary.objects.create(
+                user=user,
+                body=results["text"]
+            )
+
+    return render(
+        request,
+        "core/commentary_list.html",
+        context={
+            "commentary_list": commentaries,
+            "form": commentary_form
+        }
+    )
+
