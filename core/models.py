d07cdb74ea58fe929973fcf50723487e6d9c4f34
Roman Podolets <romanpyu@gmail.com>
2023-12-05 16:06:08 +0200

    Created User & Commentary models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=128),
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.username.__str__()
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"<Commentary from {self.user.username} at {self.created}>"
